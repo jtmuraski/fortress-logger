32d63f417a2d8e2c26f2684c2060f0a0f6d6b935
John Muraski <62039431+jtmuraski@users.noreply.github.com>
2022-12-15 07:57:40 -0600

    Init

---
+++ FortressLogger/FortressLogger/FortressLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortressLogger
+{
+    public class FortressLog
+    {
+        private static string _logPath { get; set; }
+
+        public FortressLog(string filePath)
+        {
+            _logPath = filePath;
+        }
+
+
+    }
+}
