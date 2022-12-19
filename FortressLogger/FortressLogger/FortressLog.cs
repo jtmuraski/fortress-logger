f6855500c8cd762d2ec43b386366457b5c48d3c7
John Muraski <62039431+jtmuraski@users.noreply.github.com>
2022-12-19 06:55:02 -0600

    Basic Logger Complete
    The basic functionality of the logger has been completed

--- FortressLogger/FortressLogger/FortressLog.cs
+++ FortressLogger/FortressLogger/FortressLog.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FortressLogger
@@ -9,12 +9,141 @@ namespace FortressLogger
     public class FortressLog
     {
         private static string _logPath { get; set; }
+        private static string _folderName { get; set; }
+        private static string _logDir { get; set; }
+        private static string _debuggerFile { get; set; }
+        private static string _errorFile { get; set; }
+        private static string _alertFile { get; set; }
+        private static string _infoFile { get; set; }
+        private static string _fullLogFile { get; set;}
+        private static string _whiteSpace = "   ";
 
-        public FortressLog(string filePath)
+        /// <summary>
+        /// Initialize the logger with a file path and an option name for the folder. The constructor will create the folder if it does not already exist
+        /// If a folder name is not supplied, it will default to Fortress Logger Logs.
+        /// </summary>
+        /// <param name="filePath">REQUIRED: File path for the logs to be stored. Do NOT include a '/' at the end of the filepath</param>
+        /// <param name="folderName">OPTIONAL: Name of the folder that the logs should be stored in</param>
+        public FortressLog(string filePath, string folderName = "")
         {
+            // Create the log directory if it is not already present
             _logPath = filePath;
+            _folderName = string.IsNullOrEmpty(folderName) ? "Fortress Logger Logs" : folderName;
+            _logDir = _logPath + "/" + _folderName;
+            var dirInfo = System.IO.Directory.CreateDirectory(_logDir);
+
+            // Set up File Name for levels
+            _debuggerFile = _logDir + "/Debugger.txt";
+            _errorFile = _logDir + "/Errors.txt";
+            _alertFile = _logDir + "/Alerts.txt";
+            _infoFile = _logDir + "/Info.txt";
+            _fullLogFile = _logDir + "Full Logs/FullLogs.txt";
         }
 
+        /// <summary>
+        /// Writer a log message to the Debugger File. You may supply a function name to aid in logger readability
+        /// </summary>
+        /// <param name="message">REQUIRED: THe log message being recorded</param>
+        /// <param name="functionName">OPTIONAL: The name of the method or function that the message is being recorded from. Defaults to 'Application'</param>
+        public void LogDebugger(string message, string functionName = "")
+        {
+            // Build the message
+            StringBuilder loggerLine = new StringBuilder();
+            loggerLine.Append(DateTimeOffset.Now.ToString("MM/dd/yyyy HH:mm:ss"));
+            loggerLine.Append(_whiteSpace);
+            loggerLine.Append(string.IsNullOrEmpty(functionName) ? "Application" : functionName);
+            loggerLine.Append(_whiteSpace);
+            loggerLine.Append("DEBUGGER");
+            loggerLine.Append("_whiteSpace");
+            loggerLine.Append(message);
+
+            // Write the message to the Debug file and the Full Log file
+            using (StreamWriter writer = new StreamWriter(_debuggerFile, true))
+            {
+                writer.WriteLine(loggerLine.ToString());
+            }
+
+            using (StreamWriter writer = new StreamWriter(_fullLogFile, true))
+            {
+                writer.WriteLine(loggerLine.ToString());
+            }
+            return;
+        }
+
+        public void LogError(string message, string functionName = "")
+        {
+            // Build the message
+            StringBuilder loggerLine = new StringBuilder();
+            loggerLine.Append(DateTimeOffset.Now.ToString("MM/dd/yyyy HH:mm:ss"));
+            loggerLine.Append(_whiteSpace);
+            loggerLine.Append(string.IsNullOrEmpty(functionName) ? "Application" : functionName);
+            loggerLine.Append(_whiteSpace);
+            loggerLine.Append("ERROR");
+            loggerLine.Append(_whiteSpace);
+            loggerLine.Append(message);
+
+            // Write the message to the Debug file and the Full Log file
+            using (StreamWriter writer = new StreamWriter(_errorFile, true))
+            {
+                writer.WriteLine(loggerLine.ToString());
+            }
+
+            using (StreamWriter writer = new StreamWriter(_fullLogFile, true))
+            {
+                writer.WriteLine(loggerLine.ToString());
+            }
+            return;
+        }
+
+        public void LogAlert(string message, string functionName = "")
+        {
+            // Build the message
+            StringBuilder loggerLine = new StringBuilder();
+            loggerLine.Append(DateTimeOffset.Now.ToString("MM/dd/yyyy HH:mm:ss"));
+            loggerLine.Append(_whiteSpace);
+            loggerLine.Append(string.IsNullOrEmpty(functionName) ? "Application" : functionName);
+            loggerLine.Append(_whiteSpace);
+            loggerLine.Append("ALERT");
+            loggerLine.Append(_whiteSpace);
+            loggerLine.Append(message);
+
+            // Write the message to the Debug file and the Full Log file
+            using (StreamWriter writer = new StreamWriter(_alertFile, true))
+            {
+                writer.WriteLine(loggerLine.ToString());
+            }
+
+            using (StreamWriter writer = new StreamWriter(_fullLogFile, true))
+            {
+                writer.WriteLine(loggerLine.ToString());
+            }
+            return;
+        }
+
+        public void LogInfo(string message, string functionName = "")
+        {
+            // Build the message
+            StringBuilder loggerLine = new StringBuilder();
+            loggerLine.Append(DateTimeOffset.Now.ToString("MM/dd/yyyy HH:mm:ss"));
+            loggerLine.Append(_whiteSpace);
+            loggerLine.Append(string.IsNullOrEmpty(functionName) ? "Application" : functionName);
+            loggerLine.Append(_whiteSpace);
+            loggerLine.Append("INFO");
+            loggerLine.Append(_whiteSpace);
+            loggerLine.Append(message);
+
+            // Write the message to the Debug file and the Full Log file
+            using (StreamWriter writer = new StreamWriter(_infoFile, true))
+            {
+                writer.WriteLine(loggerLine.ToString());
+            }
+
+            using (StreamWriter writer = new StreamWriter(_fullLogFile, true))
+            {
+                writer.WriteLine(loggerLine.ToString());
+            }
+            return;
+        }
 
     }
 }
---
+++ FortressLogger/LoggerUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LoggerUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void LogDebugger()
+        {
+
+        }
+    }
+}
